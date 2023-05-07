441ccefabc8fe66e9aec4f22406baba52f946205
Anton Roters <91165689+toshydev@users.noreply.github.com>
2023-05-07 18:28:31 +0200

    Update setup-win.bat

--- setup-win.bat
+++ setup-win.bat
@@ -1,10 +1,10 @@
 :: Installing Git
-ECHO "\n🧪 Installing Git..."
+ECHO "\n Installing Git..."
 ECHO "(Version control system)\n"
 
 winget install --id Git.Git -e --source winget
 
-ECHO "\n✅ Done"
+ECHO "\n Done"
 
 :: Installing GitHub CLI
 ECHO "\n🧪 Installing Command Line Tools..."
@@ -14,7 +14,7 @@ winget install --id GitHub.cli
 winget install JanDeDobbeleer.OhMyPosh -s winget
 winget install Schniz.fnm
 
-ECHO "\n✅ Done"
+ECHO "\n Done"
 
 :: Installing Win 11 apps
 ECHO "\n🧪 Installing Win 11 Apps..."
@@ -22,10 +22,10 @@ ECHO "(Visual Studio Code)\n"
 
 winget install vscode
 
-ECHO "\n✅ Done"
+ECHO "\n Done"
 
 :: Set up node lts
-ECHO "\n🧪 Installing Node LTS..."
+ECHO "\n Installing Node LTS..."
 ECHO "(JavaScript runtime for local development)\n"
 
 fnm install lts-latest
@@ -33,15 +33,15 @@ fnm use lts-latest
 ECHO "Installed Node Version:"
 node -v
 
-ECHO "\n✅ Done"
+ECHO "\n Done"
 
 :: Install global node packages
-ECHO "\n🧪 Installing Global Node Modules..."
+ECHO "\n Installing Global Node Modules..."
 ECHO "(Node Package Manager - npm)\n"
 
 npm i -g eslint
 
 ECHO "Installed global node modules:" && npm list -g --depth=0;
 
-ECHO "\n✅ Done"
+ECHO "\n Done"
 PAUSE
